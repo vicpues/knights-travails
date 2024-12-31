7c8ce4512fb94eae8dd2f514d6243f5032030d64
Vic Pues <141432283+vicpues@users.noreply.github.com>
2024-12-31 22:23:39 +0100

    docs: Write specification in JSDoc

---
+++ main.js
@@ -0,0 +1,8 @@
+/** Finds the shortest path a knight can take on a chessboard between the
+ * `start` position and the `end` position, and prints out each cell in the path.
+ * @param {[number, number]} start The cell from which to start the cell.
+ * start[0] is the column (x position), and start[1] is the row (y position).
+ * @param {[number, number]} end The cell we want to reach.
+ * start[0] is the column (x position), and start[1] is the row (y position).
+ */
+function knightMoves(start, end) {}
