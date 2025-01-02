9be98ed654f6e8e230a187d34b2014fb4f73c241
Vic Pues <141432283+vicpues@users.noreply.github.com>
2025-01-02 18:07:07 +0100

    test: Add expected printouts to main testing script

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 const knightMoves = require("./knight-moves");
 
 knightMoves([0, 0], [0, 0]);
+// You made it in 0 steps! Here's your path:
+// [ 0, 0 ]
+
 knightMoves([3, 3], [0, 0]);
+// You made it in 2 steps! Here's your path:
+// [ 3, 3 ]
+// [ 1, 2 ]
+// [ 0, 0 ]
+
 knightMoves([0, 0], [7, 7]);
+// You made it in 6 steps! Here's your path:
+// [ 0, 0 ]
+// [ 1, 2 ]
+// [ 2, 0 ]
+// [ 3, 2 ]
+// [ 4, 4 ]
+// [ 5, 6 ]
+// [ 7, 7 ]
